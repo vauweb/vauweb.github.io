c44c62b4d9d97ad0f5e87325c158795c2404a55f
Anton Ulyanov <polecat5@yandex.ru>
2019-12-11 00:34:35 +0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,52 @@
+Vue.use(VueRouter);
+const app = new Vue({
+    vuetify: new Vuetify({
+        icons: {
+            iconfont: 'mdi', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4'
+        },
+    }),
+    router:new VueRouter({
+        routes:[
+            { path: "/" },
+        ],
+        mode:"hash",
+        scrollBehavior (to, from, savedPosition) {
+            if (savedPosition) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        resolve(savedPosition);
+                    }, 200);
+                });
+            } else {
+                return { x: 0, y: 0 };
+            }
+        },
+        beforeEnter:(to,from,next)=>{
+            next();
+        }
+    }),
+    el: '#app',
+    components:{        
+    },
+    data: {
+      drawer:false
+    },
+    methods:{},
+    computed:{},
+    watch:{},
+    template:`<v-app dark>
+    <v-navigation-drawer app v-model="drawer" disable-resize-watcher>
+        <v-toolbar dense>
+            <v-btn icon title="Закрыть меню" @click.stop="drawer = !drawer"><v-icon>mdi-close</v-icon></v-btn>
+        </v-toolbar>
+    </v-navigation-drawer>
+    <v-app-bar app dense>
+        <v-btn icon title="Открыть меню" @click.stop="drawer = !drawer"><v-icon>mdi-menu</v-icon></v-btn>
+    </v-app-bar>   
+    <v-content>
+        <v-container fluid>
+            <router-view />
+        </v-container>
+    </v-content>
+</v-app>`
+});
