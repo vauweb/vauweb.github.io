bef6a76ca663c2e54885689ba46511aca1b30937
Anton Ulyanov <polecat5@yandex.ru>
2019-12-11 00:43:22 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+let redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+if (redirect && redirect != location.href) {
+    history.replaceState(null, null, redirect);
+}
+
 Vue.use(VueRouter);
 const app = new Vue({
     vuetify: new Vuetify({
